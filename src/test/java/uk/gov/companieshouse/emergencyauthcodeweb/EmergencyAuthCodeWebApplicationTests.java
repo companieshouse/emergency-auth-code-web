e6e6a2a2048e5e8b6af170ce791f317985f129ac
rsachdevch <rsachdev@companieshouse.gov.uk>
2020-05-12 14:41:55 +0100

    WIP: Skeleton web app

---
+++ src/test/java/uk/gov/companieshouse/emergencyauthcodeweb/EmergencyAuthCodeWebApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.gov.companieshouse.emergencyauthcodeweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmergencyAuthCodeWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
