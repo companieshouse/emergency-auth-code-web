0c803c25c8d7afee19dda6e7033e30c7bd08a469
Joseph-Nash <jnash@companieshouse.gov.uk>
2020-05-15 15:35:57 +0100

    add model for CompanyDetail

---
+++ src/main/java/uk/gov/companieshouse/web/emergencyauthcodeweb/model/company/CompanyDetail.java
@@ -0,0 +1,24 @@
+package uk.gov.companieshouse.web.emergencyauthcodeweb.model.company;
+
+public class CompanyDetail {
+
+    private String companyName;
+
+    private String companyNumber;
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getCompanyNumber() {
+        return companyNumber;
+    }
+
+    public void setCompanyNumber(String companyNumber) {
+        this.companyNumber = companyNumber;
+    }
+}
