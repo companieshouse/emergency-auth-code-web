a3a4e8ef3a66e86a23c126a9bb168a1fa07f0eaf
Joseph-Nash <jnash@companieshouse.gov.uk>
2020-05-18 08:34:43 +0100

    remove unnecessary imports

--- src/main/java/uk/gov/companieshouse/web/emergencyauthcodeweb/controller/eac/EACStartController.java
+++ src/main/java/uk/gov/companieshouse/web/emergencyauthcodeweb/controller/eac/EACStartController.java
@@ -2,9 +2,7 @@ package uk.gov.companieshouse.web.emergencyauthcodeweb.controller.eac;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
