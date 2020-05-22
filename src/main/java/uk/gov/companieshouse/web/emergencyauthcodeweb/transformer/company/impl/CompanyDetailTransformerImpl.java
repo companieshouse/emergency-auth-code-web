eaeb8ca37b84a48759776abe6cc430d8866bc5bf
Joseph-Nash <jnash@companieshouse.gov.uk>
2020-05-22 16:56:14 +0100

    add commons-lang3 dependency and use capitalize method on company status

--- src/main/java/uk/gov/companieshouse/web/emergencyauthcodeweb/transformer/company/impl/CompanyDetailTransformerImpl.java
+++ src/main/java/uk/gov/companieshouse/web/emergencyauthcodeweb/transformer/company/impl/CompanyDetailTransformerImpl.java
@@ -1,5 +1,6 @@
 package uk.gov.companieshouse.web.emergencyauthcodeweb.transformer.company.impl;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 import uk.gov.companieshouse.api.model.company.CompanyProfileApi;
 import uk.gov.companieshouse.web.emergencyauthcodeweb.model.company.CompanyDetail;
@@ -20,8 +21,7 @@ public class CompanyDetailTransformerImpl implements CompanyDetailTransformer {
         companyDetail.setCompanyNumber(companyProfile.getCompanyNumber());
 
         String companyStatus = companyProfile.getCompanyStatus();
-        companyDetail.setCompanyStatus(
-                companyStatus.substring(0, 1).toUpperCase() + companyStatus.substring(1));
+        companyDetail.setCompanyStatus(StringUtils.capitalize(companyStatus));
 
         companyDetail.setDateOfCreation(companyProfile.getDateOfCreation()
                 .format(DateTimeFormatter.ofPattern("dd MMMM yyyy")));
