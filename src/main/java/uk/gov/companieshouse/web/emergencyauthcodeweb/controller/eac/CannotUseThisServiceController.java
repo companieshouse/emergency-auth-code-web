563f74acc2c0353232347ece44b17663b7e6b7aa
Joseph-Nash <jnash@companieshouse.gov.uk>
2020-05-27 13:22:13 +0100

    add controller and html for unable to use service page

---
+++ src/main/java/uk/gov/companieshouse/web/emergencyauthcodeweb/controller/eac/CannotUseThisServiceController.java
@@ -0,0 +1,32 @@
+package uk.gov.companieshouse.web.emergencyauthcodeweb.controller.eac;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import uk.gov.companieshouse.web.emergencyauthcodeweb.annotation.PreviousController;
+import uk.gov.companieshouse.web.emergencyauthcodeweb.controller.BaseController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@PreviousController(CompanyConfirmationPageController.class)
+@RequestMapping("/auth-code-requests/company/{companyNumber}/cannot-use-this-service")
+public class CannotUseThisServiceController extends BaseController {
+
+    private static final String CANNOT_USE_THIS_SERVICE = "eac/cannotUseThisService";
+
+    @Override
+    protected String getTemplateName() {
+        return CANNOT_USE_THIS_SERVICE;
+    }
+
+    @GetMapping
+    public String getCompanyInformation(@PathVariable String companyNumber, Model model) {
+
+        addBackPageAttributeToModel(model, companyNumber);
+
+        return getTemplateName();
+    }
+}
