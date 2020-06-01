944fd849642fc97fdedbcded879b595abd432582
Joseph-Nash <jnash@companieshouse.gov.uk>
2020-06-01 12:35:18 +0100

    add validation for company status via transformer add unit tests rename test file

--- src/test/java/uk/gov/companieshouse/web/emergencyauthcodeweb/transformer/company/impl/CompanyDetailTransformerImplTest.java
+++ src/test/java/uk/gov/companieshouse/web/emergencyauthcodeweb/transformer/company/impl/CompanyDetailTransformerImplTest.java
@@ -1,6 +1,5 @@
 package uk.gov.companieshouse.web.emergencyauthcodeweb.transformer.company.impl;
 
-import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -24,7 +23,7 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 
 @ExtendWith(MockitoExtension.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-public class CompanyTransformerImplTest {
+public class CompanyDetailTransformerImplTest {
 
     @Mock
     private CompanyGet companyGet;
@@ -38,20 +37,27 @@ public class CompanyTransformerImplTest {
     private CompanyDetailTransformer companyDetailTransformer = new CompanyDetailTransformerImpl();
 
     private static final String COMPANY_NAME = "company";
+
     private static final String COMPANY_NUMBER = "number";
+
     private static final String COMPANY_STATUS = "status";
+
+    private static final String ACTIVE_COMPANY_STATUS = "active";
+    private static final String FORMATTED_ACTIVE_COMPANY_STATUS = "Active";
+
     private static final LocalDate DATE_OF_CREATION = LocalDate.of(2000, 01, 01);
+
     private static final String COMPANY_TYPE = "company-type";
 
-    private CompanyProfileApi createMockCompanyProfileApi() {
+    private CompanyProfileApi createMockCompanyProfileApi(String companyName, String companyNumber, String companyStatus, LocalDate doc, String companyType) {
 
         CompanyProfileApi companyProfile = new CompanyProfileApi();
 
-        companyProfile.setCompanyName(COMPANY_NAME);
-        companyProfile.setCompanyNumber(COMPANY_NUMBER);
-        companyProfile.setCompanyStatus(COMPANY_STATUS);
-        companyProfile.setDateOfCreation(DATE_OF_CREATION);
-        companyProfile.setType(COMPANY_TYPE);
+        companyProfile.setCompanyName(companyName);
+        companyProfile.setCompanyNumber(companyNumber);
+        companyProfile.setCompanyStatus(companyStatus);
+        companyProfile.setDateOfCreation(doc);
+        companyProfile.setType(companyType);
 
         return companyProfile;
     }
@@ -60,11 +66,25 @@ public class CompanyTransformerImplTest {
     @DisplayName("Get Company Detail - All fields Populated Path")
     void getCompanyDetailAllPopulated() {
 
-        CompanyDetail companyDetailReturned = companyDetailTransformer.getCompanyDetail(createMockCompanyProfileApi());
+        CompanyDetail companyDetailReturned = companyDetailTransformer.getCompanyDetail(createMockCompanyProfileApi(COMPANY_NAME, COMPANY_NUMBER, COMPANY_STATUS, DATE_OF_CREATION, COMPANY_TYPE));
 
         assertEquals(COMPANY_NAME, companyDetailReturned.getCompanyName());
         assertEquals(COMPANY_NUMBER, companyDetailReturned.getCompanyNumber());
-        assertEquals(StringUtils.capitalize(COMPANY_STATUS), companyDetailReturned.getCompanyStatus());
+        assertEquals(COMPANY_STATUS, companyDetailReturned.getCompanyStatus());
+        assertEquals(DATE_OF_CREATION.format(DateTimeFormatter.ofPattern("dd MMMM yyyy")), companyDetailReturned.getDateOfCreation());
+        assertEquals(COMPANY_TYPE, companyDetailReturned.getType());
+    }
+
+    @Test
+    @DisplayName("Get Company Detail - Active status")
+    void getCompanyDetailActiveStatus() {
+
+        CompanyDetail companyDetailReturned = companyDetailTransformer.getCompanyDetail(createMockCompanyProfileApi(COMPANY_NAME, COMPANY_NUMBER, ACTIVE_COMPANY_STATUS, DATE_OF_CREATION, COMPANY_TYPE));
+
+
+        assertEquals(COMPANY_NAME, companyDetailReturned.getCompanyName());
+        assertEquals(COMPANY_NUMBER, companyDetailReturned.getCompanyNumber());
+        assertEquals(FORMATTED_ACTIVE_COMPANY_STATUS, companyDetailReturned.getCompanyStatus());
         assertEquals(DATE_OF_CREATION.format(DateTimeFormatter.ofPattern("dd MMMM yyyy")), companyDetailReturned.getDateOfCreation());
         assertEquals(COMPANY_TYPE, companyDetailReturned.getType());
     }
