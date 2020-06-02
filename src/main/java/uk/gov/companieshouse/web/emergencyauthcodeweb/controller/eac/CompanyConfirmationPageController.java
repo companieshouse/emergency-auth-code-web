38380d80cd660fed509641aac2ad3ea933acf5b1
rsachdevch <rsachdev@companieshouse.gov.uk>
2020-06-02 15:51:58 +0100

    Add missing @PostMapping annotation

--- src/main/java/uk/gov/companieshouse/web/emergencyauthcodeweb/controller/eac/CompanyConfirmationPageController.java
+++ src/main/java/uk/gov/companieshouse/web/emergencyauthcodeweb/controller/eac/CompanyConfirmationPageController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
 import uk.gov.companieshouse.web.emergencyauthcodeweb.annotation.NextController;
@@ -69,6 +70,8 @@ public class CompanyConfirmationPageController extends BaseController {
 
         return getTemplateName();
     }
+
+    @PostMapping
     public String postListOfDirectors(@PathVariable("companyNumber") String companyNumber, HttpServletRequest request) {
         EACRequest eacRequest = new EACRequest();
         EACRequest returnedRequest;
