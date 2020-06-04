1d1d752bec1b6df0f58b054e94b3724a72714fa5
rsachdevch <rsachdev@companieshouse.gov.uk>
2020-06-04 17:52:19 +0100

    Add models for officer and officer list

--- src/main/java/uk/gov/companieshouse/web/emergencyauthcodeweb/model/emergencyauthcode/officer/EACOfficer.java
+++ src/main/java/uk/gov/companieshouse/web/emergencyauthcodeweb/model/emergencyauthcode/officer/EACOfficer.java
@@ -1,4 +1,103 @@
 package uk.gov.companieshouse.web.emergencyauthcodeweb.model.emergencyauthcode.officer;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+
+import java.time.LocalDate;
+
 public class EACOfficer {
+    @JsonProperty("id")
+    private String id;
+
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("officer_role")
+    private String officerRole;
+
+    @JsonProperty("date_of_birth")
+    private EACOfficerDOB dateOfBirth;
+
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonSerialize(using = LocalDateSerializer.class)
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    @JsonProperty("appointed_on")
+    private LocalDate appointedOn;
+
+    @JsonProperty("nationality")
+    private String nationality;
+
+    @JsonProperty("country_of_residence")
+    private String countryOfResidence;
+
+    @JsonProperty("occupation")
+    private String occupation;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOfficerRole() {
+        return officerRole;
+    }
+
+    public void setOfficerRole(String officerRole) {
+        this.officerRole = officerRole;
+    }
+
+    public EACOfficerDOB getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(EACOfficerDOB dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public LocalDate getAppointedOn() {
+        return appointedOn;
+    }
+
+    public void setAppointedOn(LocalDate appointedOn) {
+        this.appointedOn = appointedOn;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getCountryOfResidence() {
+        return countryOfResidence;
+    }
+
+    public void setCountryOfResidence(String countryOfResidence) {
+        this.countryOfResidence = countryOfResidence;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
 }
--- src/main/java/uk/gov/companieshouse/web/emergencyauthcodeweb/model/emergencyauthcode/officer/EACOfficerList.java
+++ src/main/java/uk/gov/companieshouse/web/emergencyauthcodeweb/model/emergencyauthcode/officer/EACOfficerList.java
@@ -1,4 +1,51 @@
 package uk.gov.companieshouse.web.emergencyauthcodeweb.model.emergencyauthcode.officer;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
 public class EACOfficerList {
+    @JsonProperty("items_per_page")
+    private int itemsPerPage;
+
+    @JsonProperty("start_index")
+    private int startIndex;
+
+    @JsonProperty("total_results")
+    private int totalResults;
+
+    @JsonProperty("items")
+    private List<EACOfficer> items;
+
+    public int getItemsPerPage() {
+        return itemsPerPage;
+    }
+
+    public void setItemsPerPage(int itemsPerPage) {
+        this.itemsPerPage = itemsPerPage;
+    }
+
+    public int getStartIndex() {
+        return startIndex;
+    }
+
+    public void setStartIndex(int startIndex) {
+        this.startIndex = startIndex;
+    }
+
+    public int getTotalResults() {
+        return totalResults;
+    }
+
+    public void setTotalResults(int totalResults) {
+        this.totalResults = totalResults;
+    }
+
+    public List<EACOfficer> getItems() {
+        return items;
+    }
+
+    public void setItems(List<EACOfficer> items) {
+        this.items = items;
+    }
 }
