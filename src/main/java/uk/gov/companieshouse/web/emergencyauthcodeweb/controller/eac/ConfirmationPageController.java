860a52773f102053c2019b1ac48d2e8553c90f44
rsachdevch <rsachdev@companieshouse.gov.uk>
2020-06-04 18:07:50 +0100

    Refactor: change URL to include path var and add back page attribute

--- src/main/java/uk/gov/companieshouse/web/emergencyauthcodeweb/controller/eac/ConfirmationPageController.java
+++ src/main/java/uk/gov/companieshouse/web/emergencyauthcodeweb/controller/eac/ConfirmationPageController.java
@@ -3,13 +3,14 @@ package uk.gov.companieshouse.web.emergencyauthcodeweb.controller.eac;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import uk.gov.companieshouse.web.emergencyauthcodeweb.annotation.PreviousController;
 import uk.gov.companieshouse.web.emergencyauthcodeweb.controller.BaseController;
 
 @Controller
 @PreviousController(OfficerConfirmationPageController.class)
-@RequestMapping("/auth-code-requests/requests/request_id_placeholder/confirmation")
+@RequestMapping("/auth-code-requests/requests/{requestId}/confirmation")
 public class ConfirmationPageController extends BaseController {
 
     private static final String CONFIRMATION_PAGE = "eac/confirmationPage";
@@ -20,9 +21,9 @@ public class ConfirmationPageController extends BaseController {
     }
 
     @GetMapping
-    public String getConfirmationPage(Model model) {
+    public String getConfirmationPage(@PathVariable String requestId, Model model) {
 
-        addBackPageAttributeToModel(model);
+        addBackPageAttributeToModel(model, requestId);
 
         return getTemplateName();
     }
