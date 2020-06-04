d4b041e8cc696d6a88b4986c5274904bf0eee38a
rsachdevch <rsachdev@companieshouse.gov.uk>
2020-06-04 18:05:39 +0100

    Add service to retrieve eac request and list of officers and add to model, update unit tests and add officer id in post request

--- src/main/java/uk/gov/companieshouse/web/emergencyauthcodeweb/controller/eac/ListOfDirectorsController.java
+++ src/main/java/uk/gov/companieshouse/web/emergencyauthcodeweb/controller/eac/ListOfDirectorsController.java
@@ -1,13 +1,23 @@
 package uk.gov.companieshouse.web.emergencyauthcodeweb.controller.eac;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import uk.gov.companieshouse.web.emergencyauthcodeweb.annotation.NextController;
 import uk.gov.companieshouse.web.emergencyauthcodeweb.annotation.PreviousController;
 import uk.gov.companieshouse.web.emergencyauthcodeweb.controller.BaseController;
+import uk.gov.companieshouse.web.emergencyauthcodeweb.exception.ServiceException;
+import uk.gov.companieshouse.web.emergencyauthcodeweb.model.emergencyauthcode.officer.EACOfficer;
+import uk.gov.companieshouse.web.emergencyauthcodeweb.model.emergencyauthcode.officer.EACOfficerList;
+import uk.gov.companieshouse.web.emergencyauthcodeweb.model.emergencyauthcode.request.EACRequest;
+import uk.gov.companieshouse.web.emergencyauthcodeweb.service.emergencyauthcode.EmergencyAuthCodeService;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @PreviousController(CompanyConfirmationPageController.class)
@@ -17,21 +27,40 @@ public class ListOfDirectorsController extends BaseController {
 
     private static final String LIST_OF_DIRECTORS = "eac/listOfDirectors";
 
+    @Autowired
+    private EmergencyAuthCodeService emergencyAuthCodeService;
+
     @Override
     protected String getTemplateName() {
         return LIST_OF_DIRECTORS;
     }
 
     @GetMapping
-    public String getCompanyInformation(Model model) {
+    public String getListOfDirectors(@PathVariable String requestId, Model model, HttpServletRequest request) {
+
+        try {
+            EACRequest eacRequest = emergencyAuthCodeService.getEACRequest(requestId);
+            EACOfficerList eacOfficerList = emergencyAuthCodeService.getListOfOfficers(eacRequest.getCompanyNumber());
+
+            //Add back button with company number as path variable
+            addBackPageAttributeToModel(model, eacRequest.getCompanyNumber());
+
+            model.addAttribute("eacOfficerList", eacOfficerList);
+            model.addAttribute("eacOfficer", new EACOfficer());
+        } catch (ServiceException ex) {
+            LOGGER.errorRequest(request, ex.getMessage(), ex);
+            return ERROR_VIEW;
+        }
 
         return getTemplateName();
     }
 
 
     @PostMapping
-    public String postListOfDirectors() {
+    public String postListOfDirectors(@PathVariable String requestId, @ModelAttribute("eacOfficer") EACOfficer eacOfficer) {
+        String id = eacOfficer.getId();
+        LOGGER.info("ID OF THE SELECTED OFFICER: " + id);
 
-        return navigatorService.getNextControllerRedirect(this.getClass());
+        return navigatorService.getNextControllerRedirect(this.getClass(), requestId);
     }
 }
--- src/test/java/uk/gov/companieshouse/web/emergencyauthcodeweb/controller/eac/ListOfDirectorsControllerTest.java
+++ src/test/java/uk/gov/companieshouse/web/emergencyauthcodeweb/controller/eac/ListOfDirectorsControllerTest.java
@@ -10,24 +10,35 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
+import uk.gov.companieshouse.web.emergencyauthcodeweb.model.emergencyauthcode.officer.EACOfficerList;
+import uk.gov.companieshouse.web.emergencyauthcodeweb.model.emergencyauthcode.request.EACRequest;
+import uk.gov.companieshouse.web.emergencyauthcodeweb.service.emergencyauthcode.EmergencyAuthCodeService;
 import uk.gov.companieshouse.web.emergencyauthcodeweb.service.navigation.NavigatorService;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(MockitoExtension.class)
 public class ListOfDirectorsControllerTest {
     private static final String REQUEST_ID = "abc123";
+    private static final String COMPANY_NUMBER = "12345678";
     private static final String EAC_LIST_OF_DIRECTORS_PATH =
             "/auth-code-requests/requests/" + REQUEST_ID + "/officers";
     private static final String EAC_LIST_OF_DIRECTORS_VIEW = "eac/listOfDirectors";
     private static final String MOCK_CONTROLLER_PATH = UrlBasedViewResolver.REDIRECT_URL_PREFIX + "mockControllerPath";
+    private static final String TEMPLATE_OFFICER_LIST_MODEL = "eacOfficerList";
+    private static final String TEMPLATE_INDIVIDUAL_OFFICER_MODEL = "eacOfficer";
 
     private MockMvc mockMvc;
+
+    private EACRequest eacRequest = new EACRequest();
+    private EACOfficerList eacOfficerList = new EACOfficerList();
+
+    @Mock
+    private EmergencyAuthCodeService emergencyAuthCodeService;
 
     @Mock
     private NavigatorService navigatorService;
@@ -43,9 +54,15 @@ public class ListOfDirectorsControllerTest {
     @Test
     @DisplayName("Get list of directors view - successful")
     void getRequestSuccessful() throws Exception {
+        eacRequest.setCompanyNumber(COMPANY_NUMBER);
+        when(emergencyAuthCodeService.getEACRequest(REQUEST_ID)).thenReturn(eacRequest);
+        when(emergencyAuthCodeService.getListOfOfficers(eacRequest.getCompanyNumber())).thenReturn(eacOfficerList);
+
         this.mockMvc.perform(get(EAC_LIST_OF_DIRECTORS_PATH))
                 .andExpect(status().isOk())
-                .andExpect(view().name(EAC_LIST_OF_DIRECTORS_VIEW));
+                .andExpect(view().name(EAC_LIST_OF_DIRECTORS_VIEW))
+                .andExpect(model().attributeExists(TEMPLATE_OFFICER_LIST_MODEL))
+                .andExpect(model().attributeExists(TEMPLATE_INDIVIDUAL_OFFICER_MODEL));
     }
 
     @Test
