1abe579e3ffe141258871a1eb5b002f934b1df59
rsachdevch <rsachdev@companieshouse.gov.uk>
2020-07-01 10:18:27 +0100

    Add attributes for pagination if there is more than one page

--- src/main/java/uk/gov/companieshouse/web/emergencyauthcodeweb/controller/eac/ListOfOfficersController.java
+++ src/main/java/uk/gov/companieshouse/web/emergencyauthcodeweb/controller/eac/ListOfOfficersController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import uk.gov.companieshouse.web.emergencyauthcodeweb.annotation.NextController;
 import uk.gov.companieshouse.web.emergencyauthcodeweb.annotation.PreviousController;
 import uk.gov.companieshouse.web.emergencyauthcodeweb.controller.BaseController;
@@ -20,6 +21,9 @@ import uk.gov.companieshouse.web.emergencyauthcodeweb.service.emergencyauthcode.
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Controller
 @PreviousController(CompanyConfirmationPageController.class)
@@ -28,6 +32,7 @@ import javax.validation.Valid;
 public class ListOfOfficersController extends BaseController {
 
     private static final String LIST_OF_OFFICERS = "eac/listOfOfficers";
+    private static final String EAC_OFFICER_ATTR = "eacOfficer";
 
     @Autowired
     private EmergencyAuthCodeService emergencyAuthCodeService;
@@ -38,16 +43,28 @@ public class ListOfOfficersController extends BaseController {
     }
 
     @GetMapping
-    public String getListOfOfficers(@PathVariable String requestId, Model model, HttpServletRequest request) {
+    public String getListOfOfficers(@PathVariable String requestId,
+            @RequestParam(name = "page", defaultValue = "1") int page, Model model,
+            HttpServletRequest request) {
 
         try {
             EACRequest eacRequest = emergencyAuthCodeService.getEACRequest(requestId);
-            EACOfficerList eacOfficerList = emergencyAuthCodeService.getListOfOfficers(eacRequest.getCompanyNumber());
+            EACOfficerList eacOfficerList = emergencyAuthCodeService.getListOfOfficers(eacRequest.getCompanyNumber(), page);
+            int totalPages = (int) Math.ceil((double) eacOfficerList.getTotalResults() / (double) eacOfficerList.getItemsPerPage());
+
+            if(totalPages > 1) {
+                List<Integer> pageNumbers =
+                        IntStream.rangeClosed(1, totalPages).boxed().collect(Collectors.toList());
+                model.addAttribute("pageNumbers", pageNumbers);
+                model.addAttribute("currentPage", page);
+            }
 
             addBackPageAttributeToModel(model, eacRequest.getCompanyNumber());
 
             model.addAttribute("eacOfficerList", eacOfficerList);
-            model.addAttribute("eacOfficer", new EACOfficer());
+            if(!model.containsAttribute(EAC_OFFICER_ATTR)) {
+                model.addAttribute(EAC_OFFICER_ATTR, new EACOfficer());
+            }
         } catch (ServiceException ex) {
             LOGGER.errorRequest(request, ex.getMessage(), ex);
             return ERROR_VIEW;
@@ -59,26 +76,14 @@ public class ListOfOfficersController extends BaseController {
 
     @PostMapping
     public String postListOfOfficers(@PathVariable String requestId,
-                                     @ModelAttribute("eacOfficer") @Valid EACOfficer eacOfficer,
-                                     BindingResult bindingResult,
-                                     HttpServletRequest request,
-                                     Model model) {
+            @RequestParam(name = "page", defaultValue = "1") int page,
+            @ModelAttribute("eacOfficer") @Valid EACOfficer eacOfficer, BindingResult bindingResult,
+            HttpServletRequest request, Model model) {
 
         // If an officer is not selected, reload page and show error
         if(bindingResult.hasErrors()) {
-            try {
-                EACRequest eacRequest = emergencyAuthCodeService.getEACRequest(requestId);
-                EACOfficerList eacOfficerList = emergencyAuthCodeService.getListOfOfficers(eacRequest.getCompanyNumber());
-
-                addBackPageAttributeToModel(model, eacRequest.getCompanyNumber());
-
-                model.addAttribute("eacOfficerList", eacOfficerList);
-            } catch (ServiceException ex) {
-                LOGGER.errorRequest(request, ex.getMessage(), ex);
-                return ERROR_VIEW;
-            }
-
-            return getTemplateName();
+            model.addAttribute(EAC_OFFICER_ATTR, eacOfficer);
+            return getListOfOfficers(requestId, page, model, request);
         }
 
         // Retrieve company number from API for later request
