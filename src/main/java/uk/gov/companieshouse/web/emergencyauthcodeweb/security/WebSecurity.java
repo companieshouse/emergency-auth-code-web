e8962dd6d90f406223cfa7574018eab6e4f2a90a
NeilShadrach <neil.shadrach@corryn.com>
2024-08-19 12:09:07 +0100

    JU-557

--- src/main/java/uk/gov/companieshouse/web/emergencyauthcodeweb/security/WebSecurity.java
+++ src/main/java/uk/gov/companieshouse/web/emergencyauthcodeweb/security/WebSecurity.java
@@ -1,7 +1,7 @@
 package uk.gov.companieshouse.web.emergencyauthcodeweb.security;
 
-import jakarta.servlet.Filter;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -10,6 +10,7 @@ import uk.gov.companieshouse.auth.filter.HijackFilter;
 import uk.gov.companieshouse.auth.filter.UserAuthFilter;
 import uk.gov.companieshouse.session.handler.SessionHandler;
 
+@Configuration
 @EnableWebSecurity
 public class WebSecurity {
 
@@ -17,8 +18,10 @@ public class WebSecurity {
     public SecurityFilterChain authCodeSecurityFilterChain(HttpSecurity http) throws Exception {
         http
             .securityMatcher("/auth-code-requests/start", "/auth-code-requests/accessibility-statement")
+            .csrf().disable()
             .addFilterBefore(new SessionHandler(), BasicAuthenticationFilter.class)
             .addFilterBefore(new HijackFilter(), BasicAuthenticationFilter.class);
+            ;
         return http.build();
     }
 
@@ -26,7 +29,7 @@ public class WebSecurity {
     public SecurityFilterChain eacWebSecurityFilterChain(HttpSecurity http) throws Exception {
         http
             .securityMatcher("/auth-code-requests/company/**", "/auth-code-requests/requests/**")
-            .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+            .csrf().disable()
             .addFilterBefore(new SessionHandler(), BasicAuthenticationFilter.class)
             .addFilterBefore(new HijackFilter(), BasicAuthenticationFilter.class)
             .addFilterBefore(new UserAuthFilter(), BasicAuthenticationFilter.class);
